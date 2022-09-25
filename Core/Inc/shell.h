f2194624de3fabda43bcbd1a397bfa9d47ae402f
CBAdamENSEA <adam.cheikh-brahim@ensea.fr>
2022-09-26 00:07:53 +0200

    Add a reading function and messages from the shell

--- Core/Inc/shell.h
+++ Core/Inc/shell.h
@@ -7,7 +7,7 @@
 
 #ifndef INC_SHELL_H_
 #define INC_SHELL_H_
-
+int reading_uart(char RX_buffer[50], int *reading);
 void shell(void);
 
 
--- Core/Src/shell.c
+++ Core/Src/shell.c
@@ -14,33 +14,44 @@
 
 extern int state;
 extern int shell_on;
-char RX_ch_buffer;
+
 char RX_buffer[50];
 int reading=1;
 char state_msg[3];
+int led_freq=1;
+int RX_buffer_size=0;
 
+int reading_uart(char RX_buffer[50], int *reading)
+{
+	char RX_ch_buffer;
+	int buffer_size=0;
+
+	while(*reading)
+	{
+
+		HAL_UART_Receive(&huart1,&RX_ch_buffer, 1, HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart1, &RX_ch_buffer, 1, HAL_MAX_DELAY);
+		RX_buffer[buffer_size]=RX_ch_buffer;
+		buffer_size++;
+		if (RX_ch_buffer==13)
+		{
+			RX_buffer[buffer_size-1]='\0';
+			HAL_UART_Transmit(&huart1, "\n\r", 2, HAL_MAX_DELAY);
+
+			*reading=0;
+		}
+	}
+	return buffer_size;
+}
 void shell(void)
 {
-	int buffer_size=0;
+
 	int i=0;
-	char reference_buffer[]="LED_ON";
 	while(shell_on)
 	{
-		buffer_size=0;
-		while(reading)
-		{
-			HAL_UART_Receive(&huart1,&RX_ch_buffer, 1, HAL_MAX_DELAY);
-			HAL_UART_Transmit(&huart1, &RX_ch_buffer, 1, HAL_MAX_DELAY);
-			RX_buffer[buffer_size]=RX_ch_buffer;
-			buffer_size++;
-			if (RX_ch_buffer==13)
-			{
-				RX_buffer[buffer_size-1]='\0';
-				HAL_UART_Transmit(&huart1, "\n\r", 2, HAL_MAX_DELAY);
-				//HAL_UART_Transmit(&huart1, RX_buffer, buffer_size, HAL_MAX_DELAY);
-				reading=0;
-			}
-		}
+		HAL_UART_Transmit(&huart1, "STM32--->", 9, HAL_MAX_DELAY);
+		RX_buffer_size=reading_uart(RX_buffer, &reading);
+		//HAL_UART_Transmit(&huart1, RX_buffer, RX_buffer_size, HAL_MAX_DELAY);
 
 		if (strcmp(RX_buffer,"LED_ON")==0)
 		{
@@ -49,6 +60,7 @@ void shell(void)
 			{
 				HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin,GPIO_PIN_SET);
 			}
+			HAL_UART_Transmit(&huart1, "LED turned ON\n\r", 15, HAL_MAX_DELAY);
 		}
 		else if (strcmp(RX_buffer,"LED_OFF")==0)
 		{
@@ -57,9 +69,23 @@ void shell(void)
 			{
 				HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin,GPIO_PIN_RESET);
 			}
+			HAL_UART_Transmit(&huart1, "LED turned OFF\n\r", 16, HAL_MAX_DELAY);
 		}
 
 		else if (strcmp(RX_buffer,"LED_TOGGLE")==0)
+		{
+			state=LED_TOGGLE;
+			HAL_UART_Transmit(&huart1, "Choose a frequency between 1Hz and 5000Hz: ", 43, HAL_MAX_DELAY);
+			reading=1;
+			RX_buffer_size=reading_uart(RX_buffer, &reading);
+			led_freq=atoi(RX_buffer);
+			TIM2->PSC=(int)(108000000.0/(10800*led_freq)-1);
+			HAL_UART_Transmit(&huart1, "LED toggling\n\r", 14, HAL_MAX_DELAY);
+
+
+
+		}
+		else if (strcmp(RX_buffer,"LED_FREQ")==0)
 		{
 			state=LED_TOGGLE;
 		}
